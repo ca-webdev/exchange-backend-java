397829419a4b80af713108974f269c9157931abe
Tim Lai <devtimlai@gmail.com>
2023-11-22 16:20:43 -0500

    enhance limit order price generation to be more realistic

--- src/main/java/ca/webdev/exchange/ScheduledOrderSender.java
+++ src/main/java/ca/webdev/exchange/ScheduledOrderSender.java
@@ -2,7 +2,6 @@ package ca.webdev.exchange;
 
 import ca.webdev.exchange.matching.MatchingEngine;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,16 +12,27 @@ public class ScheduledOrderSender {
 
     private final Random random = new Random();
 
+    private double price = 15.0;
+
     @Autowired
     private MatchingEngine matchingEngine;
 
-    @Autowired
-    private SimpMessagingTemplate template;
-
-    @Scheduled(fixedRate = 2000)
+    /*@Scheduled(fixedRate = 2_000)
     public void fireOrder() {
-        System.out.println(Thread.currentThread().getName() + " fireOrder is called");
+        System.out.println("fireOrder");
         matchingEngine.insertBuyLimitOrder("scheduledOrderSender", Util.round(random.nextDouble() * 10, matchingEngine.getTickSizeInPrecision()), 1);
         matchingEngine.insertSellLimitOrder("scheduledOrderSender", Util.round(random.nextDouble() * 10, matchingEngine.getTickSizeInPrecision()), 1);
+    }*/
+
+    @Scheduled(fixedRate = 2_000)
+    public void fireRandomWalkOrder() {
+        System.out.println("fire random walk orders");
+        if (random.nextBoolean()) {
+            matchingEngine.insertBuyLimitOrder("scheduledOrderSender", Util.round(price, matchingEngine.getTickSizeInPrecision()), 1);
+        } else {
+            matchingEngine.insertSellLimitOrder("scheduledOrderSender", Util.round(price, matchingEngine.getTickSizeInPrecision()), 1);
+        }
+
+        price = price + random.nextGaussian(0, 3 * matchingEngine.getTickSize());
     }
 }
--- src/main/java/ca/webdev/exchange/web/websocket/OrderBookUpdateWebSocketPublisher.java
+++ src/main/java/ca/webdev/exchange/web/websocket/OrderBookUpdateWebSocketPublisher.java
@@ -1,9 +1,9 @@
 package ca.webdev.exchange.web.websocket;
 
 import ca.webdev.exchange.listeners.OrderBookListener;
-import ca.webdev.exchange.web.model.OrderBookUpdate;
 import ca.webdev.exchange.matching.MatchingEngine;
 import ca.webdev.exchange.matching.Order;
+import ca.webdev.exchange.web.model.OrderBookUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 import java.util.Queue;
 import java.util.SortedMap;
-import java.util.stream.Collectors;
+import java.util.TreeMap;
 
 @Component
 public class OrderBookUpdateWebSocketPublisher implements OrderBookListener {
@@ -30,6 +30,13 @@ public class OrderBookUpdateWebSocketPublisher implements OrderBookListener {
     }
 
     private Map<Double, Integer> sumSizes(Map<Double, Queue<Order>> orderBook) {
-        return orderBook.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().stream().mapToInt(Order::getRemainingSize).sum()));
+        SortedMap<Double, Integer> sizeSummedOrderBook = new TreeMap<>();
+
+        orderBook.forEach((priceLevel, orders) -> {
+            int sum = orders.stream().mapToInt(Order::getRemainingSize).sum();
+            sizeSummedOrderBook.put(priceLevel, sum);
+        });
+
+        return sizeSummedOrderBook;
     }
 }
